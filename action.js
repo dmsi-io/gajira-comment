637cec73cf4037cf3c7c6cb84fafe6aa16dede55
alehechka <ajlehechka@gmail.com>
2021-12-20 21:02:59 -0600

    allowRepeats input

--- action.js
+++ action.js
@@ -16,17 +16,17 @@ module.exports = class {
   async execute() {
     const issues = this.argv.issue || this.config.issue || null;
     const { comment } = this.argv;
+    const allowRepeats = this.argv['allow-repeats'];
+    console.log('allowRepeats', allowRepeats);
 
     const issueIds = typeof issues === 'string' ? issues.split(',') : [];
     for (const issueId of issueIds) {
       const resp = await this.Jira.getIssue(issueId);
-      console.log(resp);
-      console.log(resp.fields);
-      console.log(resp.fields.comment);
-      console.log(resp.fields.comment.comments);
-      console.log(JSON.stringify(resp.fields.comment.comments));
 
-      if (resp.fields.comment.comments.find((commentObj) => commentObj.body === comment) !== undefined) {
+      if (
+        allowRepeats === 'false' &&
+        resp.fields.comment.comments.find((commentObj) => commentObj.body === comment) !== undefined
+      ) {
         console.log(`Comment already exists on issue: ${issueId}`);
         return {};
       }
